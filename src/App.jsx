1d1474404f329572610bff82ee085b9ca1f64b49
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-03-15 20:41:08 +0600

    fakedata fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 
+import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
 
+  const [recipes , SetRecipes] = useState([]);
+
+
+  useEffect(()=>{
+    const loadRecipeData = async () =>{
+      const response = await fetch('./fakedata.json');
+      const data = await response.json();
+      SetRecipes(data);
+    }
+    loadRecipeData();
+  } ,[])
+
+
+  console.log(recipes);
   return (
     <>
       
